dee8acd6086aa73aed6b4715e92ddac0eef22358
Gregory Rozhkov <grigoriyroghkov@gmail.com>
2019-01-24 01:23:32 +0200

    Deploy event_manager and recommender

--- Readme.md
+++ Readme.md
@@ -1,2 +1,20 @@
+Recommender service from the box
+================================
 
-Train model time for ecommerce dataset is 43 min.
+Ports:
+
+`5555` - event manager
+
+`5556` - recommender
+
+`5557` - job runner
+
+
+
+
+Characteristics
+===============
+
+Ecommerce dataset:
+Train model time for ecommerce dataset is 35-43 min.
+Recommendations for 12025 items
--- dev/start_dev_env.sh
+++ dev/start_dev_env.sh
@@ -4,10 +4,12 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 mkdir -p $DIR/img/tmp
 
-cp $DIR/../job_runner/target/scala-2.11/job_runner-assembly-0.1.jar $DIR/img/tmp/job_runner-assembly-0.1.jar
-cp $DIR/../export_job/target/scala-2.11/export_job-assembly-0.1.jar $DIR/img/tmp/export_job-assembly-0.1.jar
+cp $DIR/../job_runner/target/scala-2.11/job_runner-assembly-0.1.jar         $DIR/img/tmp/job_runner-assembly-0.1.jar
+cp $DIR/../export_job/target/scala-2.11/export_job-assembly-0.1.jar         $DIR/img/tmp/export_job-assembly-0.1.jar
 cp $DIR/../similarity_job/target/scala-2.11/similarity_job-assembly-0.1.jar $DIR/img/tmp/similarity_job-assembly-0.1.jar
-cp $DIR/../import_job/target/scala-2.11/import_job-assembly-0.1.jar $DIR/img/tmp/import_job-assembly-0.1.jar
+cp $DIR/../import_job/target/scala-2.11/import_job-assembly-0.1.jar         $DIR/img/tmp/import_job-assembly-0.1.jar
+cp $DIR/../event_manager/target/scala-2.11/event_manager-assembly-0.1.jar   $DIR/img/tmp/event_manager-assembly-0.1.jar
+cp $DIR/../recommender/target/scala-2.11/recommender-assembly-0.1.jar       $DIR/img/tmp/recommender-assembly-0.1.jar
 
 # TODO remove --build and build images before with included jars
 docker-compose -f "$DIR/docker-compose-dev.yaml" up -d --build $@
